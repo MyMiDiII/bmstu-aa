70e094af2b859d23283b1c01819be0ee396aaa83
MyMiDiII <mymidi.student@yandex.ru>
2021-11-19 23:51:39 +0300

    ADD: rows nums and graphs

--- hw/src/dijkstra.py
+++ hw/src/dijkstra.py
@@ -5,27 +5,27 @@ def dijkstra(graph, start):
     rows = len(graph)                                          #1
     columns = len(graph[0])                                    #2
 
-    dists = [inf] * rows
-    dists[start] = 0
+    dists = [inf] * rows                                       #3
+    dists[start] = 0                                           #4 
 
-    queue = [i for i in range(rows)]
+    queue = [i for i in range(rows)]                           #5
 
-    while queue:
-        minVal = inf
-        minInd = -1
+    while queue:                                               #6
+        minVal = inf                                           #7
+        minInd = -1                                            #8
 
-        for i, dist in enumerate(dists):
-            if dist < minVal and i in queue:
-                minVal = dist
-                minInd = i
+        for i, dist in enumerate(dists):                       #9
+            if dist < minVal and i in queue:                   #10
+                minVal = dist                                  #11
+                minInd = i                                     #12
 
-        queue.remove(minInd)
+        queue.remove(minInd)                                   #13
 
-        for i in range(columns):
-            if graph[minInd][i] and i in queue:
-                newDist = dists[minInd] + graph[minInd][i]
-                if newDist < dists[i]:
-                    dists[i] = newDist
+        for i in range(columns):                               #14
+            if graph[minInd][i] and i in queue:                #15
+                newDist = dists[minInd] + graph[minInd][i]     #16
+                if newDist < dists[i]:                         #17
+                    dists[i] = newDist                         #18
 
     return dists
 
